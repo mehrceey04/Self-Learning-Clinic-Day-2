0839cb23bba484a61a6efe1c9b151cf6594e60ec
Halimat Mercy Oseni <Halimat Mercy Oseni>
2017-02-14 06:21:52 +0100

    Added comments to codes for readability

--- app/reverseStr.js
+++ app/reverseStr.js
@@ -1,3 +1,9 @@
+/*
+  *reverseString(str)
+  *it returns the reverse of a string if its not a palindrome 
+    and if its not an empty string
+*/
+
 module.exports = {
   reverseString : function(str){
     if(str.length === 0){
