cbd0240362ea8edd57ea5b105afbebbed09f2abc
Halimat Mercy Oseni <Halimat Mercy Oseni>
2017-02-14 16:41:59 +0100

    Added api.js

--- api.js
+++ api.js
@@ -1,13 +1,37 @@
 /*
   *Consuming a Public API using HTTP client library
-  *Jokes are available at https://api.icndb.com
-  *Use HTTP GET to retrieve what you want
-  *Results are returned in JSON format
+  *Use HTTP GET to retrieve list of New York Times Best Sellersbooks, results are returned in JSON format
 */
+require('dotenv').config();
+var readlineSync = require('readline-sync');
+var request = require('request')
+var option = readlineSync.question("If you want to retrieve all best-sellers, type all. If you want to retrieve best-sellers from a particular author, type author: ");
 
-var request = require('request');
-request('http://api.icndb.com/jokes/340', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body); 
-  }
-})
+if(option == "author"){
+  var author = readlineSync.question("Enter an author's name: ");
+  //Use the request method to fetch 
+  request.get({
+  url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
+  qs:{
+    'api-key':process.env.API_KEY,
+    author:"" + author  
+  }}, function(err, response, body) {
+  body = JSON.parse(body);
+  console.log(body);
+  })
+}
+else if (option === "all") {
+  //Use the request method to fetch 
+  request.get({
+  url: "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json",
+  qs:{
+    'api-key':process.env.API_KEY
+    }}, function(err, response, body) {
+  body = JSON.parse(body);
+  console.log(body);
+  })
+}
+else{
+  console.log("You didn't type a valid answer, please try again");
+}
+
